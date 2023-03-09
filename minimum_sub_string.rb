a54ae0a049d734f110ba669d2f38ce45c83f4abb
200455939-yashu <79508991+200455939-yashu@users.noreply.github.com>
2023-03-09 15:45:12 -0500

    Add files via upload

---
+++ minimum_sub_string.rb
@@ -0,0 +1,21 @@
+s = "ADOBECODEBANC"
+t = "ABC"
+
+def minimum_sub_string(s, t)
+	i = 0 
+	j = 0
+	right = 0
+	left = s.length 
+	count = t.length
+	found = false
+
+	min_point = s.length/2
+
+	left_string =  s[0..mid_point]
+	right_string =  s[mid_point+1..t.length-1]
+
+		
+
+
+
+end
